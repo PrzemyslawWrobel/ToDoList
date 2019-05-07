3a3d2641239e1953f3230a64f546ebd3740b8bc8
PW <przemek.wrobel@poczta.fm>
2019-05-07 09:04:05 +0200

    wydzielone komponenty i kontenery

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import  React, { Component } from 'react'
-
-
-
-
-class ToDoList extends Component {
-  state = {
-    tasks: this.props.tasks,
-    draft: ''
-  }
-
-  updateDraft = (event) => {
-    this.setState({draft: event.target.value})
-  }
-
-  addToDo = () => {
-    const { tasks, draft } = this.state
-    const list = tasks 
-    list.push(draft)
-    this.setState({tasks: list, draft: ''})
-  }
-
-  render() {
-    const { title } = this.props
-    const { tasks, draft } = this.state
-    return (
-      <div>
-        <h1>{title}</h1>
-        <input type='text' onChange={this.updateDraft} value={draft}/>
-        <button onClick={this.addToDo}>Add</button>
-        {tasks.map(task =><div> <p>{task}</p></div>)}
-      </div>
-    )
-  }
-}    
+import './App.css'
+import ToDoList from '../src/Containers/ToDoList/ToDoList'
 
 class App extends Component {
-  myTasks = [
-    'Record a ReactJs video',
-    'go for a wack',
-    'blas'
-  ]
-
+  
   render() {
     return (
       <div className="App">
-        <h1>Hello, React!</h1>
-        <ToDoList title='My stuff' tasks={this.myTasks}/>
+        <h1>Hello Service</h1>
+        <ToDoList />
       </div>
     )
   }
